41f99fa95902a1a8fd4af5d9a06afbc613036000
Bekir Gülestan <gulestanbekir@gmail.com>
2025-02-08 17:18:03 -0600

    fix data = req.json

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,10 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { NextApiRequest } from "next";
+import { NextRequest } from "next/server";
 
-export async function GET(request: NextApiRequest) {
-  return new Response( (await (await clerkClient()).users.getUser(request.body.userId)).imageUrl,{
+export async function GET(request: NextRequest) {
+
+  const data = request.json();
+  return new Response( (await (await clerkClient()).users.getUser(data.userId)).imageUrl,{
     headers:{
       "Content-Type": "application/json;",
     },
