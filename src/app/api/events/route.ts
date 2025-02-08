a7d74832869705b2e2c60d73e84ace2473566f86
Bekir Gülestan <gulestanbekir@gmail.com>
2025-02-08 07:00:42 -0600

    database import

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,14 @@
+import db from "@/db";
+import { eventTable } from "@/db/schema";
+
+export async function GET(request: Request) {
+  console.log(request);
+  var events = await db.select().from(eventTable);
+  console.log(events.length);
+  return new Response(JSON.stringify(events), {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      // UTF
+    },
+  });
+}
